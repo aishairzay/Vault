cfa6573c527438ffaedcf09e08ddece08da4295e
Pratik Patel <prpatel05@gmail.com>
2023-03-17 15:37:32 -0400

    Setup Helper For Calling Scripts/Txs.

--- account/__tests__/Accounts-test.js
+++ account/__tests__/Accounts-test.js
@@ -1,7 +1,13 @@
 import { createAccount } from "../accounts";
 
-it(`creates an account`, async () => {
+// This test acutally creates an account so only run manually by adding .only (don't commit it)
+test(`creates an account`, async () => {
     let account = await createAccount();
     expect(account.address).toBeDefined();
     expect(account.address.startsWith("0x")).toBeTruthy();
+    console.log(account);
 }, 60000);
+
+test.only(`fake test`, async () => {
+    // This makes it so the test above doesn't run.
+});
