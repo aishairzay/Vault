43976db436e3057e65646d1642012e76b47c8088
Pratik Patel <prpatel05@gmail.com>
2023-03-21 15:40:23 -0400

    Fix Flow TX Issue.

--- account/__tests__/Accounts-test.js
+++ account/__tests__/Accounts-test.js
@@ -1,13 +1,11 @@
 import { createAccount } from "../accounts";
 
-// This test acutally creates an account so only run manually by adding .only (don't commit it)
 test(`creates an account`, async () => {
+    if (!process.env.LILICO_TOKEN) {
+        return;
+    }
     let account = await createAccount();
     expect(account.address).toBeDefined();
     expect(account.address.startsWith("0x")).toBeTruthy();
     console.log(account);
 }, 60000);
-
-test.only(`fake test`, async () => {
-    // This makes it so the test above doesn't run.
-});
--- flow/__tests__/FlowHelper-test.js
+++ flow/__tests__/FlowHelper-test.js
@@ -1,6 +1,6 @@
 import { FlowHelper } from "../FlowHelper";
 
-test.only(`can call script`, async () => {
+test(`can call script`, async () => {
     const flowHelper = new FlowHelper();
     const scriptResult = await flowHelper.runScript(
         "pub fun main(): Int { return 42 }"
@@ -8,8 +8,10 @@ test.only(`can call script`, async () => {
     expect(scriptResult).toEqual("42");
 });
 
-// This test requires credentials to run, set the environment variables and run manually by adding .only (don't commit it)
 test(`can call tx`, async () => {
+    if (!process.env.FLOW_ADDRESS) {
+        return;
+    }
     const flowHelper = new FlowHelper({
         address: process.env.FLOW_ADDRESS,
         privateKey: process.env.FLOW_PRIVATE_KEY,
